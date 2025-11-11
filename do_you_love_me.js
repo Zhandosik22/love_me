f703aa426c23f8fbdef2503fe48ae972650a3929
Zhandos <241382@astanait.edu.kz>
2025-11-12 03:04:03 +0500

    First commit  Do you love me project

---
+++ do_you_love_me.js
@@ -0,0 +1,27 @@
+const questionContainer = document.querySelector(".question-container");
+const resultContainer = document.querySelector(".result-container");
+const gifResult = document.querySelector(".gif-result");
+const heartLoader = document.querySelector(".cssload-main");
+const yesBtn = document.querySelector(".js-yes-btn");
+const noBtn = document.querySelector(".js-no-btn");
+
+// перемещение кнопки "No"
+noBtn.addEventListener("mouseover", () => {
+  const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
+  const newY = Math.floor(Math.random() * questionContainer.offsetHeight);
+
+  noBtn.style.left = `${newX}px`;
+  noBtn.style.top = `${newY}px`;
+});
+
+// кнопка "Yes"
+yesBtn.addEventListener("click", () => {
+  questionContainer.style.display = "none";
+  heartLoader.style.display = "inherit";
+
+  setTimeout(() => {
+    heartLoader.style.display = "none";
+    resultContainer.style.display = "inherit";
+    gifResult.play();
+  }, 3000);
+});
